07a82e1309815381053fbb8a91aaa8a347ca0b28
Flober <andrewchen14250@gmail.com>
2021-03-31 12:09:11 -0400

    ClearBlade initialization success, collection yields errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "clearblade-js-client/lib/mqttws31";
+import { ClearBlade } from "clearblade-js-client";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const cb = new ClearBlade();
+
+    cb.init({
+      URI: "https://platform.clearblade.com", // e.g., 'https://platform.clearblade.com'
+      systemKey: "86daa4860cceef81fcbeef87a469",
+      systemSecret: "86DAA4860CD8B5A7F7CDE4DFEC9101",
+      email: "test@gmail.com", // use registerEmail instead if you wish to create a new user
+      password: "test@gmail.com",
+      callback: initCallback,
+    });
+
+    function initCallback(err, cb) {
+      // err is a boolean, cb has APIs and constructors attached
+      if (err) {
+        throw new Error(cb);
+      } else {
+        console.log("success, ", { cb });
+        var collection = cb.collection();
+        /*
+        collection.fetch(someQuery, collectionFetchCallback(err, rows) {
+          if (err) {
+            throw new Error(rows);
+        } 
+        })
+        */
+      }
+    }
+  }, []);
+
+  return <div className="App">placeholder</div>;
 }
 
 export default App;

04ea6bcaf3098115f6ac69fd8d0c19dcaacbaa0e
Flober <andrewchen14250@gmail.com>
2021-03-31 18:11:41 -0400

    updated title

---
+++ src/secrets.js
@@ -0,0 +1,7 @@
+const URL = "https://platform.clearblade.com";
+const systemKey = "86daa4860cceef81fcbeef87a469";
+const systemSecret = "86DAA4860CD8B5A7F7CDE4DFEC9101";
+const email = "test@gmail.com";
+const password = "test@gmail.com";
+
+export { URL, systemKey, systemSecret, email, password };

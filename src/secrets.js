e99d12680516aa5e1c69107231f973b29a66f184
Flober <andrewchen14250@gmail.com>
2021-04-01 12:24:19 -0400

    removed secrets.js

--- src/secrets.js
+++
@@ -1,9 +0,0 @@
-// for easy to run purposes, I do not use these and add to gitignore
-
-const URL = "https://platform.clearblade.com";
-const systemKey = "86daa4860cceef81fcbeef87a469";
-const systemSecret = "86DAA4860CD8B5A7F7CDE4DFEC9101";
-const email = "test@gmail.com";
-const password = "test@gmail.com";
-
-export { URL, systemKey, systemSecret, email, password };
